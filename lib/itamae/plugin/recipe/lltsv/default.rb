dc157817d48bdd5b4573fd2c4afe6158aae44589
Takahiro Ooishi <taka0125@gmail.com>
2019-08-06 19:46:02 +0900

    :+1: リダイレクトしても取りに行くように

--- lib/itamae/plugin/recipe/lltsv/default.rb
+++ lib/itamae/plugin/recipe/lltsv/default.rb
@@ -3,7 +3,7 @@ include_recipe 'lltsv::attributes'
 execute 'install lltsv' do
   command <<-EOC
   rm -f /tmp/lltsv && \
-  curl https://github.com/sonots/lltsv/releases/download/v#{node[:lltsv][:version]}/lltsv_#{node[:lltsv][:arch]} -o /tmp/lltsv && \
+  curl -L https://github.com/sonots/lltsv/releases/download/v#{node[:lltsv][:version]}/lltsv_#{node[:lltsv][:arch]} -o /tmp/lltsv && \
   chmod a+x /tmp/lltsv && \
   mv /tmp/lltsv #{node[:lltsv][:install_path]}/lltsv
   EOC
